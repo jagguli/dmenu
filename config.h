495584f196e7125e0ca2c29924a25f2d45dd552b
Steven Joseph <steven@stevenjoseph.in>
2022-04-13 17:47:02 +1000

    Patched for centering

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Hack:size=10"
@@ -21,3 +22,4 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+static int min_width = 800;                    /* minimum width when centered */
